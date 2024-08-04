9f182ad02508da5720a6ea17456dddceab62eb7b
cnuhay <144130728+cnuhay@users.noreply.github.com>
2024-08-04 16:22:22 -0700

    change constant URLs

--- src/constants.js
+++ src/constants.js
@@ -1,10 +1,10 @@
 // Define your constants here
 
 export const READ_URL = ' https://booklibrary-ygl9.onrender.com/api/books/';
-//export const SIGNUP_URL = ' https://booklibrary-ygl9.onrender.com/api/signup/';
-//export const LOGIN_URL = ' https://booklibrary-ygl9.onrender.com/api/login/';
-export const SIGNUP_URL = ' http://localhost:5000/api/signup/';
-export const LOGIN_URL = ' http://localhost:5000/api/login/';
+export const SIGNUP_URL = ' https://booklibrary-ygl9.onrender.com/api/signup/';
+export const LOGIN_URL = ' https://booklibrary-ygl9.onrender.com/api/login/';
+//export const SIGNUP_URL = ' http://localhost:5000/api/signup/';
+//export const LOGIN_URL = ' http://localhost:5000/api/login/';
 
 
 // Add other constants as needed
