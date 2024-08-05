b1a6a4f0c3d160c1a3e1b93433c0188a00e4b329
cnuhay <144130728+cnuhay@users.noreply.github.com>
2024-08-05 12:30:01 -0700

    Updated API Links for Can

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,10 @@
 // Define your constants here
 
-export const READ_URL = ' https://booklibrary-ygl9.onrender.com/api/books/';
-export const SIGNUP_URL = ' https://booklibrary-ygl9.onrender.com/api/signup/';
-export const LOGIN_URL = ' https://booklibrary-ygl9.onrender.com/api/login/';
+//export const READ_URL = ' https://booklibrary-ygl9.onrender.com/api/books/';
+//export const SIGNUP_URL = ' https://booklibrary-ygl9.onrender.com/api/signup/';
+//export const LOGIN_URL = ' https://booklibrary-ygl9.onrender.com/api/login/';
 //export const SIGNUP_URL = ' http://localhost:5000/api/signup/';
 //export const LOGIN_URL = ' http://localhost:5000/api/login/';
+export const SIGNUP_URL = ' https://booklibraryproject.onrender.com/api/signup/';//Can API Links
+export const LOGIN_URL = ' https://booklibraryproject.onrender.com/api/login/';//Can API Links
+export const READ_URL = ' https://booklibraryproject.onrender.com/api/books/';//Can API Links
